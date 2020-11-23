0312b708406f924d3169eaa73e68e2ecbd9da559
zumyu <34504759+zumyu@users.noreply.github.com>
2020-11-24 08:05:04 +0900

    [remove] exceptionの削除

--- io/zumyu/midorin/mapconfig/exception/GameExistException.java
+++
@@ -1,9 +0,0 @@
-package io.zumyu.midorin.mapconfig.exception;
-
-public class GameExistException extends Exception
-{
-   public GameExistException(String gameName)
-   {
-      super(gameName + "　is already using this plugin.");
-   }
-}
--- io/zumyu/midorin/mapconfig/exception/config/ConfigException.java
+++
@@ -1,9 +0,0 @@
-package io.zumyu.midorin.mapconfig.exception.config;
-
-public class ConfigException extends Exception
-{
-   public ConfigException()
-   {
-      super();
-   }
-}
