3bc06ee856a5d7192bb62821b3372d1efe83d2ae
zumyu <34504759+zumyu@users.noreply.github.com>
2020-11-28 09:24:56 +0900

    Create LogHelper.java

---
+++ io/zumyu/midorin/mapconfig/util/LogHelper.java
@@ -0,0 +1,13 @@
+package io.zumyu.midorin.mapconfig.util;
+
+import io.zumyu.midorin.mapconfig.MapConfigPlugin;
+
+import java.util.logging.Level;
+
+public final class LogHelper
+{
+   public static void log(Level level, String msg)
+   {
+      MapConfigPlugin.getInstance().getLogger().log(level, msg);
+   }
+}
