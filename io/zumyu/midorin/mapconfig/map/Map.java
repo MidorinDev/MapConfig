f7c6c0074dab764fcfd497b2244305e067880fc6
zumyu <34504759+zumyu@users.noreply.github.com>
2020-11-28 09:24:24 +0900

    Update マップのエディター機能を追加

--- io/zumyu/midorin/mapconfig/map/Map.java
+++ io/zumyu/midorin/mapconfig/map/Map.java
@@ -1,64 +1,51 @@
 package io.zumyu.midorin.mapconfig.map;
 
-import io.zumyu.midorin.mapconfig.util.Logger;
-import io.zumyu.midorin.mapconfig.exception.config.TeamNotExistException;
 import org.bukkit.Location;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.Plugin;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Objects;
-import java.util.logging.Level;
+import java.io.IOException;
+import java.util.*;
 
 public class Map
 {
-   private File mapFile;
+   private final File mapFile;
    private String mapName;
    private String worldName;
    private Location worldSpawnPoint;
-   private List<String> teams = new ArrayList<>();
-   private java.util.Map<String, Location> teamSpawnPoints = new HashMap<>();
+   private List<String> teams;
+   private java.util.Map<String, Location> teamSpawnPoints;
 
+//   public Map(File mapFile) throws MapConfigException
    public Map(File mapFile)
    {
       this.mapFile = mapFile;
 
-      if (mapFile.exists())
+      if (mapFile.exists() & mapFile.getName().endsWith(".yml"))
       {
          final FileConfiguration mapFileConfig = YamlConfiguration.loadConfiguration(mapFile);
 
-         if (mapFileConfig.contains("mapName") &&
-                 mapFileConfig.contains("worldName") &&
-                 mapFileConfig.contains("worldSpawnPoint") &&
-                 mapFileConfig.contains("team") &&
-                 mapFileConfig.contains("teamSpawnPoint"))
-         {
-            mapName = mapFileConfig.getString("mapName");
-            worldName = mapFileConfig.getString("worldName");
-            worldSpawnPoint = (Location) mapFileConfig.get("worldSpawnPoint");
-            teams = mapFileConfig.getStringList("team");
+         mapName = mapFileConfig.getString("mapName");
+         worldSpawnPoint = (Location) mapFileConfig.get("worldSpawnPoint");
+         worldName = worldSpawnPoint.getWorld().getName();
+         teams = mapFileConfig.getStringList("team");
+         teamSpawnPoints = new HashMap<>();
 
-            for (String team : teams)
-            {
-               if (mapFileConfig.contains("teamSpawnPoint." + team))
-               {
-                  teamSpawnPoints.put(team, (Location) mapFileConfig.get("teamSpawnPoint." + team));
-               }
-            }
-         }
-         else
+         for (String team : teams)
          {
-            Logger.log(Level.INFO, this.mapFile.getName() + " の記述に問題があります");
+            if (mapFileConfig.contains("teamSpawnPoint." + team))
+            {
+               teamSpawnPoints.put(team, (Location) mapFileConfig.get("teamSpawnPoint." + team));
+            }
          }
       }
    }
 
    /**
     * マップ名取得
-    * @return mapName
+    * @return mapName マップ名
     */
    public String getMapName()
    {
@@ -67,7 +54,7 @@ public class Map
 
    /**
     * ワールド名取得
-    * @return worldName
+    * @return worldName ワールド名
     */
    public String getWorldName()
    {
@@ -76,7 +63,7 @@ public class Map
 
    /**
     * 初期スポーン座標取得
-    * @return worldSpawnPoint
+    * @return worldSpawnPoint 初期スポーン
     */
    public Location getWorldSpawnPoint()
    {
@@ -85,7 +72,7 @@ public class Map
 
    /**
     * チームのリスト取得
-    * @return teams
+    * @return teams チームリスト
     */
    public List<String> getTeams()
    {
@@ -95,13 +82,12 @@ public class Map
    /**
     * チームのスポーン座標取得
     * @param team
-    * @return teamSpawnPoint
-    * @throws TeamNotExistException
+    * @return teamSpawnPoint チームが存在していれば、座標を返す。無ければ、null
     */
-   public Location getTeamSpawnPoint(String team) throws TeamNotExistException
+   public Location getTeamSpawnPoint(String team)
    {
       if (teamSpawnPoints.containsKey(team)) return teamSpawnPoints.get(team);
-      else throw new TeamNotExistException(this, team);
+      return null;
    }
 
    @Override
@@ -124,41 +110,107 @@ public class Map
       return Objects.hash(mapFile, mapName, worldName, worldSpawnPoint, teams, teamSpawnPoints);
    }
 
-   public class Builder
+   public static class Editor
    {
-      private File mapFile;
+      private final Plugin plugin;
       private String mapName;
       private String worldName;
       private Location worldSpawnPoint;
-      private List<String> teams = new ArrayList<>();
-      private java.util.Map<String, Location> teamSpawnPoints = new HashMap<>();
+      private final List<String> teams = new ArrayList<>();
+      private final java.util.Map<String, Location> teamSpawnPoints = new HashMap<>();
+
+      public Editor(Plugin plugin)
+      {
+         this.plugin = plugin;
+      }
 
       public void setMapName(String mapName)
       {
          this.mapName = mapName;
       }
 
-      public void setWorldName(String worldName)
-      {
-         this.worldName = worldName;
-      }
-
       public void setWorldSpawnPoint(Location worldSpawnPoint)
       {
          this.worldSpawnPoint = worldSpawnPoint;
+         this.worldName = this.worldSpawnPoint.getWorld().getName();
       }
 
-      public boolean setTeam(String teamName, Location spawnPoint)
+      public boolean addTeam(String teamName, Location spawnPoint)
       {
          if (!teams.contains(teamName))
          {
             teams.add(teamName);
-            if (teamSpawnPoints.containsKey(teamName)) teamSpawnPoints.remove(teamName);
             teamSpawnPoints.put(teamName, spawnPoint);
 
             return true;
          }
          else return false;
+      }
+
+      public boolean removeTeam(String teamName)
+      {
+         if (teams.contains(teamName))
+         {
+            teams.remove(teamName);
+            teamSpawnPoints.remove(teamName);
+
+            return true;
+         }
+         else return false;
+      }
+
+      public void create()
+      {
+         File file = new File(plugin.getDataFolder() + "/map/" + mapName + ".yml");
+         FileConfiguration fileConfig = YamlConfiguration.loadConfiguration(file);
+         fileConfig.options().header("Do not edit this file.");
+         fileConfig.set("mapName", mapName);
+         fileConfig.set("worldName", worldName);
+         fileConfig.set("worldSpawnPoint", worldSpawnPoint);
+         fileConfig.set("team", teams);
+         for (String team : teams)
+         {
+            if (teamSpawnPoints.containsKey(team)) fileConfig.set("teamSpawnPoint." + team, teamSpawnPoints.get(team));
+         }
+
+         try
+         {
+            fileConfig.save(file);
+         }
+         catch (IOException exception)
+         {
+            exception.printStackTrace();
+         }
+      }
+
+      public Plugin getPlugin()
+      {
+         return plugin;
+      }
+
+      public String getMapName()
+      {
+         return mapName;
+      }
+
+      public String getWorldName()
+      {
+         return worldName;
+      }
+
+      public Location getWorldSpawnPoint()
+      {
+         return worldSpawnPoint;
+      }
+
+      public List<String> getTeams()
+      {
+         return teams;
+      }
+
+      public java.util.Map<String, Location> getTeamSpawnPoints()
+      {
+         return teamSpawnPoints;
       }
    }
 }
